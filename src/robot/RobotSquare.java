d9cdcf00a5b2ded7dfe000470868f0ae7630688e
danielasuarez <daniela.suarez@gompersstudent.org>
2017-04-17 14:51:37 -0700

    Add files via upload

--- src/robot/RobotSquare.java
+++ src/robot/RobotSquare.java
@@ -6,22 +6,24 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotSquare {
     public static void main(String[] args) throws Exception {
         // 1. Make a new Robot
-
+Robot liza = new Robot();
 
         // 3. Put the robot's pen down
-
+liza.penDown();
 
         // 6. Make the robot move as fast as possible
 
-
+liza.setSpeed(500);
         // 5. Do everything below here 4 times
+for (int i = 0; i < 4; i++) {
+	
 
 
         //         2. Move your robot 200 pixels
-
+liza.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
-
+liza.turn(90);
+}
     }
 }
---
+++ src/robot/liza.java
@@ -0,0 +1,38 @@
+package robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class liza {
+public static void main(String[] args) {
+	Robot liza = new Robot();
+	liza.setSpeed(10);
+	liza.turn(270);
+	liza.move(500);
+	liza.turn(90);
+	liza.penDown();
+	liza.move(150);
+	liza.turn(120);
+    liza.move(150);
+    liza.setSpeed(10);
+    liza.turn(120);
+    liza.move(150);
+    liza.penUp();
+    liza.turn(210);
+    liza.move(150);
+    liza.turn(300);
+    liza.penDown();
+    liza.move(150);
+    liza.turn(120);
+    liza.move(75);
+    liza.turn(120);
+    liza.move(75);
+    liza.turn(180);
+    liza.move(75);
+    liza.turn(60);
+    liza.move(75);
+    liza.turn(300);
+    liza.penUp();
+    liza.move(150);
+  
+}
+}
